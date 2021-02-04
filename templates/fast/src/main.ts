de0e9509c82abcdd7b933bea01f5e0d62461588b
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-02-03 17:30:33 -0800

    Move the FAST Element template files into the new fast/element directory

--- templates/fast/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { CounterCard } from "./components/counter-card";
-
-/*
- * Ensure that tree-shaking doesn't remove these components from the bundle.
- * There are multiple ways to prevent tree shaking, of which this is one.
- */
-CounterCard;
